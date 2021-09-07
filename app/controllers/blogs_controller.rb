0d15e460b98fa2e46d47b6aa65f3dc662c8d1c40
taiki.ishikawa.97@gmail.com <ec2-user@ip-172-31-8-81.ec2.internal>
2021-09-07 09:40:29 +0000

    [Add] 削除機能の追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -30,9 +30,12 @@ class BlogsController < ApplicationController
   end
 
   def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to  blogs_path
   end
-  
-  
+
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
